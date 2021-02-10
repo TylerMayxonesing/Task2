5939c3d97b433fbb2fddd52a45a2221b313a20d5
tyler <tmaxyonesing@ucdavis.edu>
2021-02-09 19:32:22 -0800

    Initial Commit

---
+++ Task2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task2
+//
+//  Created by Tyler on 2/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
